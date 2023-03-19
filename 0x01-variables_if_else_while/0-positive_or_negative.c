f8efceffea538f592f39ce838070659d6651c0b7
raphael <erbynn1234@gmail.com>
2023-03-19 10:41:14 -0700

    check number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print string
+ *
+ * Description: use printf to print string
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int n = rand();
+	if(n > 0)
+	{
+		puts("is positive\n");
+	}
+	else if(n < 0)
+	{
+		puts("is negative\n");
+	}
+	else
+	{
+		puts("is zero\n");
+	}
+	return (0);
+}
