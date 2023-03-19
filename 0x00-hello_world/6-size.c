c79181c263a157da531b26d0821909d9ae64e52c
raphael <erbynn1234@gmail.com>
2023-03-19 06:12:51 -0700

    print with printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /**
+ * main - print
+ * 
  * Description: print size of types
+ * Return: 0 for success
  */
 int main(void)
 {
@@ -9,6 +12,6 @@ int main(void)
 	printf("Size of a int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	printf("Size of a float: %ld byte(s)\n ", sizeof(float));
 	return (0);
 }
