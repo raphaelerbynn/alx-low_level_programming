56b6afdbb58812963eeeca71447062828a4ec73e
raphael <erbynn1234@gmail.com>
2023-03-21 22:39:45 -0700

    hmm

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print with func
+ *
+ * Description:using  function for printing
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char a;
+
+	while(a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
