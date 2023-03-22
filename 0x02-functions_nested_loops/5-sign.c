568e560c42688220ab11c6133135eb0775d09563
raphael <erbynn1234@gmail.com>
2023-03-21 23:00:03 -0700

    hmm

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print with func
+ *
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
