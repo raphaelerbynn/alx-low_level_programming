d596b59c3ab1bfddb17995ba66a8fbb0c563094c
raphael <erbynn1234@gmail.com>
2023-03-22 21:48:13 -0700

    hmm

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,30 +12,19 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i > 99; i++)
+		for (i = n; i < 98; i++)
+
 		{
-			if (i != 98)
-			{
-				printf("%d, ", i);
-			}
-			else
-			{
-				printf("%d\n", i);
-			}
+			printf("%d, ", i);
 		}
 	}
 	else
 	{
-		for (i = n; i <= 98; i--)
+		for (i = n; i > 98; i--)
 		{
-			if (i != 98)
-			{
-				printf("%d, ", i);
-			}
-			else
-			{
-				printf("%d\n", i);
-			}
+			printf("%d, ", i);
 		}
 	}
+
+	printf("98\n");
 }
