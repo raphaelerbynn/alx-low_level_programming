17b37a309e1fca1a4b367a8f6cbe0b53b33549f0
raphael <erbynn1234@gmail.com>
2023-03-23 20:18:36 -0700

    hmm

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ *
+ * @n: param to count
+ * Description: check if c is digit
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
