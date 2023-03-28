e4cd1dcaddd6d4ea8e55857cb77c2f744feff13f
raphael <erbynn1234@gmail.com>
2023-03-27 19:45:36 -0700

    hmm

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values
+ *
+ * @a: pointer value
+ * @b: pointer value
+ * Description: swap values
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = *a;
+}
