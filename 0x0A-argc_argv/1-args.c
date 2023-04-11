bf0f30c3942d46fa5652c4495c8529b3f168feb8
raphael <erbynn1234@gmail.com>
2023-04-10 19:43:15 -0700

    hmm

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return 0;
+}
+
