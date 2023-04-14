883eb5d11d2885b0c4e1045c34f92b67bb5d2358
raphael <erbynn1234@gmail.com>
2023-04-13 22:07:41 -0700

    hmm

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,44 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * _realloc - reallocates a memory block
- * @ptr: pointer to the memory previously allocated with malloc
- * @old_size: size of the allocated space for ptr
- * @new_size: new size of the memory block
+ * _realloc - Reallocates a memory block using malloc and free
+ * @ptr: Pointer to the memory previously allocated with malloc
+ * @old_size: Size of the allocated space for ptr
+ * @new_size: New size of the memory block
  *
- * Return: pointer to the reallocated memory block
+ * Return: Pointer to the newly allocated memory block, or NULL on failure
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    if (new_size == 0 && ptr != NULL)
-    {
-        free(ptr);
-        return NULL;
-    }
+	void *new_ptr;
 
-    if (ptr == NULL)
-    {
-        return malloc(new_size);
-    }
+	/* If new_size is equal to old_size, do nothing and return ptr */
+	if (new_size == old_size)
+		return (ptr);
 
-    if (new_size == old_size)
-    {
-        return ptr;
-    }
+	/* If ptr is NULL, equivalent to malloc(new_size) */
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		return (new_ptr);
+	}
 
-    void *new_ptr;
-    unsigned int min_size = (old_size < new_size) ? old_size : new_size;
+	/* If new_size is 0, equivalent to free(ptr) */
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-    new_ptr = malloc(new_size);
-    if (new_ptr == NULL)
-    {
-        return NULL;
-    }
+	/* Allocate new memory block with new_size */
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
 
-    memcpy(new_ptr, ptr, min_size);
-    free(ptr);
+	/* Copy contents to the new memory block */
+	if (new_size > old_size)
+	{
+		memcpy(new_ptr, ptr, old_size);
+	}
+	else
+	{
+		memcpy(new_ptr, ptr, new_size);
+	}
 
-    return new_ptr;
+	/* Free the old memory block */
+	free(ptr);
+
+	return (new_ptr);
 }
+
