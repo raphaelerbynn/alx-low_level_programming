592d3254c343bf046f4fafa53efcd3f68305d5ac
unknown <erbynn1234@gmail.com>
2023-07-11 07:54:22 +0000

    test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc; /* Unused parameter */
+	printf("%s\n", argv[0]);
+	return 0;
+}
