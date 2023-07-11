6f033772ace201ab60723b7b0848e4c28a875f56
unknown <erbynn1234@gmail.com>
2023-07-11 07:55:34 +0000

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	(void)argc; /* Unused parameter */
-	printf("%s\n", argv[0]);
-	return 0;
+		(void)argc; /* Unused parameter */
+		printf("%s\n", argv[0]);
+		return 0;
 }
